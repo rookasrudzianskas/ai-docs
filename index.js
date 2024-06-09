d35d33000a984a2efb0c954ec5338f41e9d74f53
rokas <rokas@rokas.com>
2024-06-09 11:15:20 +0300

    Add gitignore, vcs.xml, import front-matter, update package.json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-console.log('Running');
-
+import fm from "front-matter";
 const parseExpoDocs = async (slug) => {
   const url = `https://raw.githubusercontent.com/expo/expo/main/docs/pages/${slug}.mdx`;
   const response = await fetch(url);
   const content = await response.text();
-  console.log(content);
+  const data = fm(content)
+  return dat;
 }
 
 parseExpoDocs('/get-started/start-developing');
